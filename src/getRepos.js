f7193405ca3abc9a43d57534c6cb455297fad7b5
Daniel Souza <danielsouzaT99@hotmail.com>
2024-10-08 20:38:32 -0300

    Atualização de conteúdo

--- src/getRepos.js
+++ src/getRepos.js
@@ -5,63 +5,69 @@ const loadingModel = document.querySelector("#repository-list-load"); // Element
 
 // Função que faz a leitura do JSON do GitHub
 const getReposJson = async function () {
-    let content = [];
+  let content = [];
 
-    // Adicione os projetos diretamente ao objeto content
-    content.unshift(
-        {
-            name: "Solubot",
-            html_url: "/",//"https://solubot.com.br",
-            description: "Sistema de multiatendimento desenvolvido em TypeScript, JavaScript, React e outras tecnologias.",
-            language: "TypeScript",
-        },
-        {
-            name: "devAPI",
-            html_url: "/",//"https://dev.solubot.com.br/docs",
-            description: "API para integração com WhatsApp.",
-            language: "TypeScript",
-        },
-    );
+  // Adicione os projetos diretamente ao objeto content
+  content.unshift(
+    {
+      name: "CorpERP - backend",
+      html_url: null,//"https://solubot.com.br",
+      description: "Sistema de ERP desenvolvido em Python, DRF e outras tecnologias.",
+      language: "Python",
+    },
+    {
+      name: "Solubot",
+      html_url: null,//"https://solubot.com.br",
+      description: "Sistema de multiatendimento desenvolvido em TypeScript, JavaScript, React e outras tecnologias.",
+      language: "TypeScript",
+    },
+    {
+      name: "devAPI",
+      html_url: null,//"https://dev.solubot.com.br/docs",
+      description: "API para integração com WhatsApp.",
+      language: "TypeScript",
+    },
+  );
 
-    for (let i = 1; i <= maxPages; i++) {
-        let apiGithub = await fetch(`https://api.github.com/users/${userName}/repos?&sort=pushed&per_page=100&page=${i}`);
-        if (apiGithub.status == 200) {
-            let data = await apiGithub.json();
-            content = content.concat(data);
-        }
+  for (let i = 1; i <= maxPages; i++) {
+    let apiGithub = await fetch(`https://api.github.com/users/${userName}/repos?&sort=pushed&per_page=100&page=${i}`);
+    if (apiGithub.status == 200) {
+      let data = await apiGithub.json();
+      content = content.concat(data);
     }
+  }
 
-    showRepositories(content); // Chama a função que irá criar a apresentação em HTML para ficar visível no site
+  showRepositories(content); // Chama a função que irá criar a apresentação em HTML para ficar visível no site
 };
 getReposJson();
 
 // Apresentação dos dados do JSON no HTML
 const showRepositories = function (data) {
-    for (const repository of data) {
-        loadingModel.style.display = 'none'; // Oculta o elemento de carregamento após o retorno do JSON
-        let listItem = document.createElement('li');
-        listItem.classList.add('repository');
-        listItem.innerHTML = `<a href=${repository.html_url}><div>
+  for (const repository of data) {
+    loadingModel.style.display = 'none'; // Oculta o elemento de carregamento após o retorno do JSON
+    let listItem = document.createElement('li');
+    listItem.classList.add('repository');
+    listItem.innerHTML = `${repository.html_url?`<a href=${repository.html_url}>`:''}<div>
             <h5>${repository.name}</h5>
             <div class="separator"></div>
             <span>${repository.description ?? ""}</span> <br/><br/>
             <span>${devicons[repository.language]/*Adiciona o ícone da respectiva linguagem */}</span> <br /></div>
             </a>`;
-        repositoryList.append(listItem); // Adiciona o conteúdo do JSON ao objeto do HTML
-    }
+    repositoryList.append(listItem); // Adiciona o conteúdo do JSON ao objeto do HTML
+  }
 }
 
 // Ícones das linguagens dos projetos
 const devicons = {
-    JavaScript: '<i class="devicon-javascript-plain devicon-icon"></i> Javascript',
-    TypeScript: '<i class="devicon-typescript-plain devicon-icon"></i> Typescript',
-    Python: '<i class="devicon-python-plain devicon-icon"></i> Python',
-    Jinja: '<i class="devicon-python-plain devicon-icon"></i> Python',
-    Java: '<i class="devicon-java-plain devicon-icon"></i> Java',
-    HTML: '<i class="devicon-html5-plain devicon-icon"></i> HTML',
-    CSS: '<i class="devicon-css3-plain devicon-icon"></i> CSS',
-    PHP: '<i class="devicon-php-plain devicon-icon"></i> PHP',
-    Rust: '<i class="devicon-rust-plain devicon-icon"></i> Rust',
-    null: '<i class="devicon-markdown-original devicon-icon"></i> Markdown',
-    undefined: ''
+  JavaScript: '<i class="devicon-javascript-plain devicon-icon"></i> Javascript',
+  TypeScript: '<i class="devicon-typescript-plain devicon-icon"></i> Typescript',
+  Python: '<i class="devicon-python-plain devicon-icon"></i> Python',
+  Jinja: '<i class="devicon-python-plain devicon-icon"></i> Python',
+  Java: '<i class="devicon-java-plain devicon-icon"></i> Java',
+  HTML: '<i class="devicon-html5-plain devicon-icon"></i> HTML',
+  CSS: '<i class="devicon-css3-plain devicon-icon"></i> CSS',
+  PHP: '<i class="devicon-php-plain devicon-icon"></i> PHP',
+  Rust: '<i class="devicon-rust-plain devicon-icon"></i> Rust',
+  null: '<i class="devicon-markdown-original devicon-icon"></i> Markdown',
+  undefined: ''
 }
--- src/stacks.js
+++ src/stacks.js
@@ -2,120 +2,148 @@ const stacksList = document.querySelector('.skill-list');//Elemento que ficará 
 const skillList = document.querySelector('.progressbar-container');//Elemento que ficará a lista de barra de habilidades
 //Lista de Habilidades
 const techStack = [
-    {
-        Language:"HTML",
-        Skill:"80%"
-    },
-    {
-        Language:"CSS",
-        Skill:"80%"
-    },
-    {
-        Language:"JavaScript",
-        Skill:"70%"
-    }, {
-        Language:"TypeScript",
-        Skill:"70%"
-    },
-    {
-        Language:"Python",
-        Skill:"75%"
-    },
-    {
-        Language:"PHP",
-        Skill:"20%"
-    },
-    {
-        Language:"Java",
-        Skill:"18%"
-    },
-    {
-        Language:"C#",
-        Skill:"6%"
-    },
-    {
-        Language:"ReactJS",
-        Skill:"60%"
-    },
-    {
-        Language:"NodeJS",
-        Skill:"60%"
-    },
-    {
-        Language:"Rust",
-        Skill:"40%"
-    }, 
-{
-        Language:"Actix-Web",
-        Skill:"35%"
-    },
-    {
-        Language:"Tkinter",
-        Skill:"35%"
-    },
-    {
-        Language:"Flask",
-        Skill:"45%"
-    },
-    {
-        Language:"Pandas",
-        Skill:"30%"
-    },
-    {
-        Language:"MySQL",
-        Skill:"60%"
-    },
-    {
-        Language:"WordPress",
-        Skill:"60%"
-    },
-    {
-        Language:"Bootstrap",
-        Skill:"60%"
-    },
-    {
-        Language:"Photoshop",
-        Skill:"40%"
-    },
-    {
-        Language:"GIMP",
-        Skill:"60%"
-    },
-    {
-        Language:"Linux",
-        Skill:"70%"
-    },
-    {
-        Language:"Windows",
-        Skill:"65%"
-    }
+  {
+    Language: "HTML",
+    Skill: "80%"
+  },
+  {
+    Language: "CSS",
+    Skill: "80%"
+  },
+  {
+    Language: "JavaScript",
+    Skill: "90%"
+  }, {
+    Language: "TypeScript",
+    Skill: "95%"
+  },
+  {
+    Language: "Python",
+    Skill: "75%"
+  },
+  {
+    Language: "PHP",
+    Skill: "20%"
+  },
+  {
+    Language: "Java",
+    Skill: "18%"
+  },
+  {
+    Language: "C#",
+    Skill: "6%"
+  },
+  {
+    Language: "ReactJS",
+    Skill: "80%"
+  },
+  {
+    Language: "NodeJS",
+    Skill: "95%"
+  },
+  {
+    Language: "Sequelize ORM",
+    Skill: "70%"
+  },
+  {
+    Language: "Prisma ORM",
+    Skill: "40%"
+  },
+  {
+    Language: "Rust",
+    Skill: "30%"
+  },
+  {
+    Language: "Actix-Web",
+    Skill: "20%"
+  },
+  {
+    Language: "Diesel-rs ORM",
+    Skill: "20%"
+  },
+  {
+    Language: "Tkinter",
+    Skill: "35%"
+  },
+  {
+    Language: "Django",
+    Skill: "35%"
+  },
+  {
+    Language: "Django REST Framework",
+    Skill: "50%"
+  },
+  {
+    Language: "Flask",
+    Skill: "45%"
+  },
+  {
+    Language: "Pandas",
+    Skill: "30%"
+  },
+  {
+    Language: "SQL Alchemy ORM",
+    Skill: "40%"
+  },
+  {
+    Language: "MySQL",
+    Skill: "60%"
+  },
+  {
+    Language: "Docker",
+    Skill: "30%"
+  },
+  {
+    Language: "GIT",
+    Skill: "70%"
+  },
+  {
+    Language: "WordPress",
+    Skill: "60%"
+  },
+  {
+    Language: "Bootstrap",
+    Skill: "60%"
+  },
+  {
+    Language: "Material - UI",
+    Skill: "70%"
+  },
+  {
+    Language: "Linux",
+    Skill: "70%"
+  },
+  {
+    Language: "Windows",
+    Skill: "65%"
+  }
 
 ]
 
 //Função que exibe as habilidades no HTML
-const showStacks = function(data){
-    
-    // Ordena a lista de habilidades pela porcentagem de habilidade
-    data.sort((a, b) => parseInt(b.Skill) - parseInt(a.Skill));
-    
-    //Para cada item da lista será criado as tags
-    for(const skill of data){
-        let listItem = document.createElement('span');
-        listItem.classList.add('skill-item');
-        listItem.innerHTML = `${skill.Language}`;
-        stacksList.append(listItem);
-    }
-    //Para cada item da lista de habilidades será criado as barras de progressos
-    for(const skill of data){
-        let listItem = document.createElement('div');
-        listItem.classList.add('skill-progressbar');
-        listItem.innerHTML =   `<label>${skill.Language}</label>
+const showStacks = function (data) {
+
+  // Ordena a lista de habilidades pela porcentagem de habilidade
+  data.sort((a, b) => parseInt(b.Skill) - parseInt(a.Skill));
+
+  //Para cada item da lista será criado as tags
+  for (const skill of data) {
+    let listItem = document.createElement('span');
+    listItem.classList.add('skill-item');
+    listItem.innerHTML = `${skill.Language}`;
+    stacksList.append(listItem);
+  }
+  //Para cada item da lista de habilidades será criado as barras de progressos
+  for (const skill of data) {
+    let listItem = document.createElement('div');
+    listItem.classList.add('skill-progressbar');
+    listItem.innerHTML = `<label>${skill.Language}</label>
                                 <div class="progress">
                                     <div class="progress-bar" role="progressbar" aria-label="Basic example" style="width: ${skill.Skill}" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
                                 </div>`;
 
-        skillList.append(listItem);
-    }
+    skillList.append(listItem);
+  }
 }
 showStacks(techStack);//Executa a função
 
